fa47265b7c16b5d9348c940ec30b702094fdcab3
Prince Singh <princesingh282002@gmail.com>
2024-02-11 00:18:11 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SchedulerApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import com.example.demo.Service.Impl.ScheduleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	@Autowired
+	ScheduleServiceImpl scheduleService;
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+
+
+	}
+}
+
---
+++ src/main/java/com/example/demo/Utility/PairClass.java
@@ -0,0 +1,14 @@
+package com.example.demo.Utility;
+
+import com.example.demo.Entity.Schedule;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+public class PairClass {
+    Date key;
+    String value;
+    long id;
+    Schedule schedule;
+}
